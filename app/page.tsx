e3e084f202e5de7ad370fe0080a17cc4ce8ec2ca
Andrey <andreydenisov25@gmail.com>
2026-03-31 18:17:53 +0300

    dev version warning

--- app/page.tsx
+++ app/page.tsx
@@ -30,12 +30,12 @@ export default function Home() {
       />
       <div className="bg-zinc-900/20 border border-zinc-600/30 backdrop-blur-md backdrop-saturate-200 rounded-3xl w-full max-w-screen-md p-3 flex items-center gap-3 shadow -mb-10">
         <div className="flex flex-col w-full">
-          <span className="text-lg lg:text-2xl font-bold">Привет, Product Radar!</span>
+          <span className="text-lg lg:text-2xl font-bold">Привет. Ты попал в DEV версию Ancial на React</span>
           <span className="text-sm lg:text-base text-zinc-300">
-            Мы вас ждали, но «ремонт» ещё продолжается.
+            Здесь отсутствует множество функций, дизайн может быть сломан, а производительность будет ужасной. Но ты можешь помочь нам найти баги и недоработки, чтобы сделать Ancial лучше!
           </span>
           <span className="text-xs lg:text-sm text-zinc-400">
-            Тут есть Лента, Чаты, Звонки, Музыка, Игры и Кошелёк, а ещё — множество недоработок)
+            Вернись в полную и стабильную версию, если не хочешь сталкиваться с проблемами и багами. Спасибо за понимание!
           </span>
           <div className="flex items-center justify-end gap-3 mt-1.5">
             <a
@@ -47,12 +47,12 @@ export default function Home() {
               <img className="w-3.5 h-3.5" src="/img/socials/tg.png" alt="Telegram" />
             </a>
             <a
-              href=""
+              href="https://ancial.ru/"
               target="_blank"
               rel="noopener noreferrer"
-              className="hidden w-fit text-center px-1 py-0.5 bg-purple-500 hover:bg-purple-600 duration-300 cursor-pointer rounded-3xl border border-zinc-600/30 active:scale-95"
+              className="w-fit text-center px-1 py-0.5 bg-purple-500 hover:bg-purple-600 duration-300 cursor-pointer rounded-3xl border border-zinc-600/30 active:scale-95"
             >
-              Оценить
+              В полную версию
             </a>
           </div>
         </div>
