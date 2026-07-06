41ed627fab5d8d9ceca193b418f33b95c272d62e
Andrey <andreydenisov25@gmail.com>
2026-07-07 00:59:57 +0300

    feat: forward search parameters to the external weather redirect URL

--- app/apps/included/weather/page.tsx
+++ app/apps/included/weather/page.tsx
@@ -1,5 +1,27 @@
 import { redirect } from 'next/navigation';
 
-export default function IncludedWeatherPage() {
-  redirect('https://ancial.ru/apps/included/weather.php');
+type IncludedWeatherPageProps = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export default async function IncludedWeatherPage({
+  searchParams,
+}: IncludedWeatherPageProps) {
+  const resolvedParams = await searchParams;
+  const queryString = new URLSearchParams();
+
+  for (const [key, value] of Object.entries(resolvedParams)) {
+    if (value !== undefined) {
+      if (Array.isArray(value)) {
+        value.forEach((val) => queryString.append(key, val));
+      } else {
+        queryString.append(key, value);
+      }
+    }
+  }
+
+  const query = queryString.toString();
+  const dest = `/apps/included/weather.php${query ? `?${query}` : ''}`;
+
+  redirect(dest);
 }
