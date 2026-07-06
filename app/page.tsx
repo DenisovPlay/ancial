97fa69fed3b8aea0a2b77b5020a7a6c86505dbf2
Andrey <andreydenisov25@gmail.com>
2026-07-06 18:59:22 +0300

    refactor: replace hardcoded homepage content with dynamic dashboard component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,12 +30,12 @@ export default function Home() {
       />
       <div className="bg-zinc-900/20 border border-zinc-600/30 backdrop-blur-md backdrop-saturate-200 rounded-3xl w-full max-w-screen-md p-3 flex items-center gap-3 shadow -mb-10">
         <div className="flex flex-col w-full">
-          <span className="text-lg lg:text-2xl font-bold">Привет. Ты попал в DEV версию Ancial на React.</span>
+          <span className="text-lg lg:text-2xl font-bold">Свершилось! Ancial теперь на React!</span>
           <span className="text-sm lg:text-base text-zinc-300">
-            Здесь отсутствует множество функций, дизайн может быть сломан, а производительность будет ужасной. Но ты можешь помочь нам найти баги и недоработки, чтобы сделать Ancial лучше!
+            Мы всё ещё продолжаем разработку, поэтому вы можете встречать баги и недоработки.
           </span>
           <span className="text-xs lg:text-sm text-zinc-400">
-            Вернись в полную и стабильную версию, если не хочешь сталкиваться с проблемами и багами. Спасибо за понимание!
+            Совсем скоро мы вернём привычный функционал, а пока наслаждайтесь обновлённым интерфейсом!
           </span>
           <div className="flex items-center justify-end gap-3 mt-1.5">
             <a
@@ -45,14 +45,6 @@ export default function Home() {
               className="w-7 h-7 flex items-center justify-center rounded-full bg-blue-400 hover:bg-blue-500 duration-300 active:scale-95 shadow border border-zinc-600/30"
             >
               <img className="w-3.5 h-3.5" src="/img/socials/tg.png" alt="Telegram" />
-            </a>
-            <a
-              href="https://ancial.ru/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="w-fit text-center px-1 py-0.5 bg-purple-500 hover:bg-purple-600 duration-300 cursor-pointer rounded-3xl border border-zinc-600/30 active:scale-95"
-            >
-              В полную версию
             </a>
           </div>
         </div>
