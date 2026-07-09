e2af3730111a3c59e06807008830046b304b97d0
Andrey <andreydenisov25@gmail.com>
2026-07-10 00:32:36 +0300

    feat: add Yandex Metrika analytics tracking to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import IconSprite from './components/icon-sprite';
 import MainContent from './components/main-content';
@@ -56,6 +57,29 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-black text-white">
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+        
+            ym(95401676, 'init', {
+                clickmap:true, 
+                referrer: document.referrer, 
+                url: location.href, 
+                accurateTrackBounce:true, 
+                trackLinks:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/95401676" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
         <IconSprite />
         <SWRegister />
         <NotificationProvider>
