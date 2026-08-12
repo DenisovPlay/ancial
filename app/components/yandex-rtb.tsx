c9c300213f01b570d634b9b47081510950799671
Andrey <andreydenisov25@gmail.com>
2026-08-12 20:37:14 +0300

    fix: remove hidden class from Yandex RTB container to ensure visibility

--- app/components/yandex-rtb.tsx
+++ app/components/yandex-rtb.tsx
@@ -71,7 +71,7 @@ export default function YandexRtb({
     <div
       className={
         className ??
-        'hidden w-full max-h-24 block overflow-hidden shadow-lg duration-300 cursor-pointer'
+        'w-full max-h-24 block overflow-hidden shadow-lg duration-300 cursor-pointer'
       }
     >
       <a
